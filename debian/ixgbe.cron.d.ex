3a1d3334bf5d89bb37f0dde72487ceaae6e4bee8
Mhamed Bouaziz <m-hamed-externe.bouaziz@edf.fr>
2018-12-06 17:18:44 +0100

    delete .ex files

--- debian/ixgbe.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the ixgbe package
-#
-0 4	* * *	root	[ -x /usr/bin/ixgbe_maintenance ] && /usr/bin/ixgbe_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(ixgbe):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="ixgbe" command="/usr/bin/ixgbe"
